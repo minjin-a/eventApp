a4e8e3139591b8022b5dc92fb500c8b541573f2c
minjin-a <tvhairshop@gmail.com>
2025-04-02 17:07:16 +0900

    테스트 파일 업데이트 2025-04-02 17:07:16

--- test-event.js
+++ test-event.js
@@ -1,3 +1,3 @@
 // 테스트 파일
-window.testTimestamp = 1743580922;
-console.log("jsDelivr 캐시 테스트 - 타임스탬프: 1743580922");
+window.testTimestamp = 1743581236;
+console.log("jsDelivr 캐시 테스트 - 타임스탬프: 1743581236");
